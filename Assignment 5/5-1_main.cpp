6251bbe31890d6d40979235bda66259c14bad8a5
yali30814936 <kevin30814936@gmail.com>
2025-03-19 01:25:40 +0800

    Update 5-1_main.cpp

--- Assignment 5/5-1_main.cpp
+++ Assignment 5/5-1_main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include "ds_q5.hpp"
 
+using namespace std;
+
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	DamageList_s *head = nullptr, *tail = nullptr;
 	int m, n, L;
 
-	std::cin >> m >> n >> L;
+	cin >> m >> n >> L;
 
 	for (int i = 0; i < L; i++) {
 		int dmg;
-		scanf("%d", &dmg);
+		cin >> dmg;
 		if (head == nullptr) {
 			head = new DamageList_s;
 			head->damage = dmg;
@@ -23,13 +28,13 @@ int main() {
 		}
 	}
 
-	std::vector<std::vector<int>> damage = visualizeDamage(head, m, n);
+	vector<vector<int>> damage = visualizeDamage(head, m, n);
 
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++) {
-			printf("%d ", damage[i * n + j]);
+			cout << damage[i][j] << " ";
 		}
-		printf("\n");
+		cout << "\n";
 	}
 	for (int i = 0; i < L; i++) {
 		DamageList_s *tmp = head;
